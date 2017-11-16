060ba020f6a5f7e82ce80040b9eae3cb26f8a7ba
Ruslan Steiger <steiger.ruslan@gmail.com>
2017-11-16 17:02:30 +0100

    Lesson 8: Components within Components

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+Vue.component('task-list', {
+    template: `
+        <ul>
+            <task v-for="task in tasks" v-text="task.description"></task>
+        </ul>`,
+    data() {
+        return {
+            tasks: [
+                { description: 'Milch kaufen', completed: true},
+                { description: 'Brot backen', completed: true},
+                { description: 'Zimmer putzen', completed: true},
+                { description: 'Auto in der Werkstatt bringen', completed: true},
+                { description: 'Elducation fertig machen', completed: false},
+                { description: 'Vue.js verstehen', completed: false}
+            ]
+        }
+    }
+});
+
 Vue.component('task', {
    template:'<li><slot></slot></li>'
 });
