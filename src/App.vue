56ebeb0e34d2ddc71c9b20d8711f03a0a574a197
Ruslan Steiger <steiger.ruslan@gmail.com>
2017-11-17 21:36:46 +0100

    Lesson 16: Webpack and vue-cli

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <message>Hello there! Lorem ipsum dolor.</message>
+        <message>WTf Lorem ipsum dolor sit amet.</message>
+    </div>
+</template>
+
+<script>
+    import Message from './components/Message.vue';
+
+    export default {
+        name: 'app',
+        components: { Message },
+        data () {
+            return {
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="box">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+    .box {
+        background: lime;
+        padding: 25px;
+        border: 1px solid limegreen;
+
+        & + & {
+            margin-top: 25px;
+        }
+    }
+
+</style>
