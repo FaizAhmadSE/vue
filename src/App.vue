d70be3fca4664013dbad72a4fb20aabaf3a62798
Ruslan Steiger <steiger.ruslan@gmail.com>
2017-11-20 10:50:48 +0100

    Lesson 17: Hot Module Replacement

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,17 @@
     <div id="app">
         <message>Hello there! Lorem ipsum dolor.</message>
         <message>WTf Lorem ipsum dolor sit amet.</message>
+        <hr>
+        <counter></counter>
     </div>
 </template>
 
 <script>
     import Message from './components/Message.vue';
-
+    import Counter from './components/Counter.vue';
     export default {
         name: 'app',
-        components: { Message },
-        data () {
-            return {
-            }
-        }
+        components: { Message, Counter },
     }
 </script>
 
